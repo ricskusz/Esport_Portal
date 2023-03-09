6aea2f4806afcdcdad6987aa3d1d7647717980b5
Richard <richarszab56@gmail.com>
2023-03-09 18:18:29 +0100

    Alap backend

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
